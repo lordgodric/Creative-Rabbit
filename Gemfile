f6b409e2659324f05eba27e1b5e2d0b925d09e3a
lordgodric <elric@ex.ua>
2016-05-01 02:37:49 +0300

    added devise customers

--- Gemfile
+++ Gemfile
@@ -60,3 +60,4 @@ group :development do
 end
 
 gem 'bootstrap4_form_builder'
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :customers
   resources :posts
   resources :homepage
   root 'homepage#index'
