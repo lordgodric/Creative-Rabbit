ee141900e252a1ace22f90a5233c361143313330
Volodymyr <elric@ex.ua>
2016-05-05 14:11:58 +0300

    add some style

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -4,5 +4,6 @@ class Designer < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  
+  has_many :logos
+
 end
