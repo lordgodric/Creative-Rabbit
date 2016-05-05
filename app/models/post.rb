399489c0d10534042582a167497adc487480b68a
Volodymyr <elric@ex.ua>
2016-05-05 15:41:02 +0300

    styled index posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_attached_file :old_logo, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/missing.png"
+  has_attached_file :old_logo, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/missing_:style.png"
   validates_attachment_content_type :old_logo, content_type: /\Aimage\/.*\Z/
   validates_presence_of :customer
   belongs_to :customer
