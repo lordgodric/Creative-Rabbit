115e7063d763b26d40b9cbc9a2137c8a98eeaf59
lordgodric <elric@ex.ua>
2016-05-29 22:55:44 +0300

    styled new_logo

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,5 +1,5 @@
 class Logo < ActiveRecord::Base
-  has_attached_file :new_logo, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/missing_:style.png"
+  has_attached_file :new_logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing_:style.png"
   validates_attachment_content_type :new_logo, content_type: /\Aimage\/.*\Z/
   belongs_to :post
 end
