31ce46dc3d5b431cb106ceeb4c51b22211918e0d
godric <elric@ex.ua>
2016-06-19 00:07:10 +0300

    fixed some right for edit

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,6 +1,9 @@
 class Logo < ActiveRecord::Base
   has_attached_file :new_logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing_:style.png"
   validates_attachment_content_type :new_logo, content_type: /\Aimage\/.*\Z/
+  validates :designer_id, presence: true
+  validates :post_id, presence: true
+
 
   belongs_to :post
   belongs_to :designer
