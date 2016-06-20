715ed23972a4f2043da8c7c98fe0ef7a5e44b488
Sergey Ryabov <connacte@gmail.com>
2016-06-20 16:23:58 +0300

    Add administrate gem

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'pry-rails'
+gem "administrate", "~> 0.2.2"
 
 
 
---
+++ app/dashboards/post_dashboard.rb
@@ -0,0 +1,72 @@
+require "administrate/base_dashboard"
+
+class PostDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    customer: Field::BelongsTo,
+    logos: Field::HasMany,
+    id: Field::Number,
+    title: Field::String,
+    content: Field::Text,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    old_logo_file_name: Field::String,
+    old_logo_content_type: Field::String,
+    old_logo_file_size: Field::Number,
+    old_logo_updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :customer,
+    :logos,
+    :id,
+    :title,
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :customer,
+    :logos,
+    :id,
+    :title,
+    :content,
+    :created_at,
+    :updated_at,
+    :old_logo_file_name,
+    :old_logo_content_type,
+    :old_logo_file_size,
+    :old_logo_updated_at,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :customer,
+    :logos,
+    :title,
+    :content,
+    :old_logo_file_name,
+    :old_logo_content_type,
+    :old_logo_file_size,
+    :old_logo_updated_at,
+  ].freeze
+
+  # Overwrite this method to customize how posts are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(post)
+  #   "Post ##{post.id}"
+  # end
+end
